83280d94415a70e7ec8f9b043d79d1e28e7296ee
Marko Krstic <mailme@markokrstic.com>
2024-04-24 17:11:27 +0200

    ..

--- dblocks-codepro.php
+++ dblocks-codepro.php
@@ -5,7 +5,8 @@
  * Requires at least: 6.5.2
  * Requires PHP:      7.0
  * Version:           1.0.0
- * Author:            The WordPress Contributors
+ * Author:            DPlugins
+ * * Author URI:      https://dplugins.com/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       dblocks-codepro
