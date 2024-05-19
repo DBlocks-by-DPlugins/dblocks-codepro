4f65d9d006b92d56b77ae2c5c6546998de8f26d9
Marko Krstic <mailme@markokrstic.com>
2024-05-19 22:24:25 +0200

    ...

--- src/edit.js
+++ src/edit.js
@@ -1,9 +1,9 @@
 import { useEffect, useRef, useState } from '@wordpress/element';
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import { Panel, PanelBody, ToggleControl, SelectControl, __experimentalUnitControl as UnitControl } from '@wordpress/components';
+import { useBlockProps } from '@wordpress/block-editor';
 import { RawHTML } from '@wordpress/element';
 import { emmetHTML } from 'emmet-monaco-es';
 import BlockControlsComponent from './component/BlockControls.js';
+import InspectorControlsComponent from './component/InspectorControlsComponent.js';
 
 import './editor.scss';
 
@@ -201,77 +201,21 @@ export default function Edit({ attributes, setAttributes }) {
 
     return (
         <>
-            <InspectorControls>
-                <Panel>
-                    <PanelBody title="Element Settings">
-                        <ToggleControl
-                            label="Use Wrapper"
-                            checked={attributes.useWrapper}
-                            onChange={() => toggleAttribute('useWrapper', !attributes.useWrapper)}
-                        />
-                    </PanelBody>
-                    <PanelBody title="Syntax Highlighting">
-                        <ToggleControl
-                            label="Activate Syntax Highlighting"
-                            checked={syntaxHighlight}
-                            onChange={() => {
-                                setSyntaxHighlight(!syntaxHighlight);
-                                toggleAttribute('syntaxHighlight', !syntaxHighlight);
-                            }}
-                        />
-                        {syntaxHighlight && (
-                            <>
-                                <ToggleControl
-                                    label="FrontEnd Dark Theme"
-                                    checked={syntaxHighlightTheme === "dark"}
-                                    onChange={toggleSyntaxHighlightTheme}
-                                />
-                                <SelectControl
-                                    label="Language"
-                                    value={editorLanguage}
-                                    options={[
-                                        { label: "HTML", value: "html" },
-                                        { label: "CSS", value: "css" },
-                                        { label: "SCSS", value: "scss" },
-                                        { label: "JavaScript", value: "js" },
-                                        { label: "PHP", value: "php" },
-                                        { label: "TypeScript", value: "typescript" },
-                                        { label: "Bash", value: "bash" },
-                                        { label: "Twig", value: "twig" },
-                                        { label: "YAML", value: "yaml" },
-                                        { label: "Plaintext", value: "plaintext" },
-                                        { label: "JSON", value: "json" },
-                                    ]}
-                                    onChange={changeEditorLanguage}
-                                />
-                            </>
-                        )}
-                    </PanelBody>
-                    <PanelBody title="Editor Global Settings">
-                        <ToggleControl
-                            label="Dark Mode"
-                            checked={theme === 'vs-dark'}
-                            onChange={toggleTheme}
-                        />
-                        <UnitControl
-                            label="Font Size"
-                            value={fontSize}
-                            onChange={(newSize) => updateAttribute('editorFontSize', newSize, '/wp-json/dblocks-codepro/v1/editor-font-size/')}
-                            units={[{ value: 'px', label: 'Pixels', default: 14 }]}
-                            min={10}
-                            max={30}
-                        />
-                        <UnitControl
-                            label="Editor Height"
-                            value={editorHeight}
-                            onChange={(newHeight) => updateAttribute('editorHeight', newHeight, '/wp-json/dblocks-codepro/v1/editor-height/')}
-                            units={[{ value: 'vh', label: 'Viewport Height', default: 50 }]}
-                            min={10}
-                            max={100}
-                        />
-                    </PanelBody>
-                </Panel>
-            </InspectorControls>
+            <InspectorControlsComponent
+                attributes={attributes}
+                setAttributes={setAttributes}
+                syntaxHighlight={syntaxHighlight}
+                setSyntaxHighlight={setSyntaxHighlight}
+                syntaxHighlightTheme={syntaxHighlightTheme}
+                toggleSyntaxHighlightTheme={toggleSyntaxHighlightTheme}
+                editorLanguage={editorLanguage}
+                changeEditorLanguage={changeEditorLanguage}
+                theme={theme}
+                toggleTheme={toggleTheme}
+                fontSize={fontSize}
+                updateAttribute={updateAttribute}
+                editorHeight={editorHeight}
+            />
 
             <div {...useBlockProps()}>
                 <BlockControlsComponent viewMode={viewMode} setViewMode={setViewMode} />
