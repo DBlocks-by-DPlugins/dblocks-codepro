c847a01c3c1f2fee3047ef2a25ebdff5fc05868c
Marko Krstic <mailme@markokrstic.com>
2024-05-19 21:15:04 +0200

    Update edit.js

--- src/edit.js
+++ src/edit.js
@@ -1,13 +1,7 @@
 import { useEffect, useRef, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import {
-	Panel,
-	PanelBody,
-	ToggleControl,
-	SelectControl,
-	__experimentalUnitControl as UnitControl,
-} from "@wordpress/components";
+import { Panel, PanelBody, ToggleControl, SelectControl, __experimentalUnitControl as UnitControl } from '@wordpress/components';
 import { RawHTML } from '@wordpress/element';
 import { emmetHTML } from 'emmet-monaco-es';
 import BlockControlsComponent from './component/BlockControls.js';
@@ -21,15 +15,9 @@ export default function Edit({ attributes, setAttributes }) {
     const [viewMode, setViewMode] = useState(initialViewMode); // Manage the view mode
     const [theme, setTheme] = useState(attributes.theme || 'vs-light');
     const [fontSize, setFontSize] = useState(attributes.editorFontSize || '14px');
-	const [syntaxHighlight, setSyntaxHighlight] = useState(
-		attributes.syntaxHighlight,
-	);
-	const [syntaxHighlightTheme, setSyntaxHighlightTheme] = useState(
-		attributes.syntaxHighlightTheme || "light",
-	);
-	const [editorLanguage, setEditorLanguage] = useState(
-		attributes.editorLanguage || "html",
-	);
+    const [syntaxHighlight, setSyntaxHighlight] = useState(attributes.syntaxHighlight);
+    const [syntaxHighlightTheme, setSyntaxHighlightTheme] = useState(attributes.syntaxHighlightTheme || "light");
+    const [editorLanguage, setEditorLanguage] = useState(attributes.editorLanguage || "html");
 
     const editorContainerRef = useRef(null);
     const editorInstanceRef = useRef(null);
@@ -39,37 +27,34 @@ export default function Edit({ attributes, setAttributes }) {
     };
 
     const toggleSyntaxHighlightTheme = async () => {
-		const newSyntaxTheme = syntaxHighlightTheme === "light" ? "dark" : "light";
-		try {
-			const response = await fetch(
-				"/wp-json/dblocks-codepro/v1/syntax-theme/",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						"X-WP-Nonce": wpApiSettings.nonce,
-					},
-					body: JSON.stringify({ syntaxTheme: newSyntaxTheme }),
-				},
-			);
+        const newSyntaxTheme = syntaxHighlightTheme === "light" ? "dark" : "light";
+        try {
+            const response = await fetch("/wp-json/dblocks-codepro/v1/syntax-theme/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-WP-Nonce": wpApiSettings.nonce,
+                },
+                body: JSON.stringify({ syntaxTheme: newSyntaxTheme }),
+            });
 
-			if (!response.ok) throw new Error("Network response was not ok.");
-			setSyntaxHighlightTheme(newSyntaxTheme); // Update local state for instant UI update
-			setAttributes({ syntaxHighlightTheme: newSyntaxTheme }); // Update block attributes for persistence
-		} catch (error) {
-			console.error("Failed to update syntax theme:", error);
-		}
-	};
+            if (!response.ok) throw new Error("Network response was not ok.");
+            setSyntaxHighlightTheme(newSyntaxTheme); // Update local state for instant UI update
+            setAttributes({ syntaxHighlightTheme: newSyntaxTheme }); // Update block attributes for persistence
+        } catch (error) {
+            console.error("Failed to update syntax theme:", error);
+        }
+    };
 
-	const toggleSyntaxHighlight = () => {
-		setSyntaxHighlight(!syntaxHighlight);
-		setAttributes({ syntaxHighlight: !syntaxHighlight });
-	};
+    const toggleSyntaxHighlight = () => {
+        setSyntaxHighlight(!syntaxHighlight);
+        setAttributes({ syntaxHighlight: !syntaxHighlight });
+    };
 
-	const changeEditorLanguage = (language) => {
-		setEditorLanguage(language);
-		setAttributes({ editorLanguage: language });
-	};
+    const changeEditorLanguage = (language) => {
+        setEditorLanguage(language);
+        setAttributes({ editorLanguage: language });
+    };
 
     const toggleTheme = async () => {
         const newTheme = theme === 'vs-light' ? 'vs-dark' : 'vs-light';
@@ -160,9 +145,9 @@ export default function Edit({ attributes, setAttributes }) {
             editorInstanceRef.current = monaco.editor.create(editorContainerRef.current, {
                 minimap: { enabled: false },
                 value: content || '<!-- some comment -->',
-                language: 'html',
+                language: editorLanguage,
                 automaticLayout: true,
-                theme: theme,
+                theme: syntaxHighlight ? `vs-${syntaxHighlightTheme}` : theme,
                 fontSize: parseInt(fontSize),
             });
 
@@ -210,7 +195,7 @@ export default function Edit({ attributes, setAttributes }) {
                 editorInstanceRef.current = null;
             }
         };
-    }, [viewMode, theme, fontSize]); // Re-run this effect whenever viewMode, theme, or fontSize changes
+    }, [viewMode, theme, fontSize, syntaxHighlight, syntaxHighlightTheme, editorLanguage]); // Re-run this effect whenever these dependencies change
 
     // Update the editor content if the `content` attribute changes
     useEffect(() => {
@@ -251,55 +236,49 @@ export default function Edit({ attributes, setAttributes }) {
                         />
                     </PanelBody>
                     <PanelBody title="Syntax Highlighting">
-						<ToggleControl
-							label="Activate Syntax Highlighting"
-							checked={syntaxHighlight}
-							onChange={toggleSyntaxHighlight}
-						/>
-						{syntaxHighlight && (
-							<>
-								<ToggleControl
-									label="Dark Theme"
-									checked={syntaxHighlightTheme === "dark"}
-									onChange={toggleSyntaxHighlightTheme}
-								/>
-								<SelectControl
-									label="Language"
-									value={editorLanguage}
-									options={[
-										{ label: "HTML", value: "html" },
-										{ label: "CSS", value: "css" },
-										{ label: "SCSS", value: "scss" },
-										{ label: "JavaScript", value: "js" },
-										{ label: "PHP", value: "php" },
-										{ label: "TypeScript", value: "typescript" },
-										{ label: "Bash", value: "bash" },
-										{ label: "Twig", value: "twig" },
-										{ label: "YAML", value: "yaml" },
-										{ label: "Plaintext", value: "plaintext" },
-										{ label: "JSON", value: "json" },
-									]}
-									onChange={changeEditorLanguage}
-								/>
-							</>
-						)}
-					</PanelBody>
+                        <ToggleControl
+                            label="Activate Syntax Highlighting"
+                            checked={syntaxHighlight}
+                            onChange={toggleSyntaxHighlight}
+                        />
+                        {syntaxHighlight && (
+                            <>
+                                <ToggleControl
+                                    label="Dark Theme"
+                                    checked={syntaxHighlightTheme === "dark"}
+                                    onChange={toggleSyntaxHighlightTheme}
+                                />
+                                <SelectControl
+                                    label="Language"
+                                    value={editorLanguage}
+                                    options={[
+                                        { label: "HTML", value: "html" },
+                                        { label: "CSS", value: "css" },
+                                        { label: "SCSS", value: "scss" },
+                                        { label: "JavaScript", value: "js" },
+                                        { label: "PHP", value: "php" },
+                                        { label: "TypeScript", value: "typescript" },
+                                        { label: "Bash", value: "bash" },
+                                        { label: "Twig", value: "twig" },
+                                        { label: "YAML", value: "yaml" },
+                                        { label: "Plaintext", value: "plaintext" },
+                                        { label: "JSON", value: "json" },
+                                    ]}
+                                    onChange={changeEditorLanguage}
+                                />
+                            </>
+                        )}
+                    </PanelBody>
                 </Panel>
             </InspectorControls>
 
             <div {...useBlockProps()}>
-            {syntaxHighlight ? (
-                 <div ref={editorContainerRef} style={{ height: '50vh' }} />
-                ) : (
-                    <>
-                        <BlockControlsComponent viewMode={viewMode} setViewMode={setViewMode} />
-                        {viewMode === 'preview' && <RawHTML>{content}</RawHTML>}
-                        {viewMode === 'split' && <RawHTML>{content}</RawHTML>}
-                        {(viewMode === 'code' || viewMode === 'split') && (
-                            <div ref={editorContainerRef} style={{ height: '50vh' }} />
-                        )}
-                    </>
-                )}    
+                <BlockControlsComponent viewMode={viewMode} setViewMode={setViewMode} />
+                {viewMode === 'preview' && <RawHTML>{content}</RawHTML>}
+                {viewMode === 'split' && <RawHTML>{content}</RawHTML>}
+                {(viewMode === 'code' || viewMode === 'split') && (
+                    <div ref={editorContainerRef} style={{ height: '50vh' }} />
+                )}
             </div>
         </>
     );
