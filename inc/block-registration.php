fbdd710b909e68754a1a89840a9ba4573441ada3
Marko Krstic <mailme@markokrstic.com>
2025-04-26 20:21:58 +0200

    if not init we get errors in editor

--- inc/block-registration.php
+++ inc/block-registration.php
@@ -8,6 +8,11 @@ function dblocks_codepro_register_block() {
 add_action('init', 'dblocks_codepro_register_block');
 
 function dblocks_codepro_enqueue_editor_assets() {
+    // Only load scripts in admin/editor context
+    if (!is_admin()) {
+        return;
+    }
+    
     wp_enqueue_script(
         'dblocks-codepro-editor-script',
         plugin_dir_url(__FILE__) . '../build/index.js',
@@ -15,8 +20,12 @@ function dblocks_codepro_enqueue_editor_assets() {
         null,
         true
     );
-    wp_localize_script('dblocks-codepro-editor-script', 'DBlocksData', array(
-        'restUrl' => rest_url('dblocks_codepro/v1/'),
-    ));
+    
+    // Only localize script in editor context
+    if (wp_script_is('wp-editor') || wp_script_is('wp-block-editor')) {
+        wp_localize_script('dblocks-codepro-editor-script', 'DBlocksData', array(
+            'restUrl' => rest_url('dblocks_codepro/v1/'),
+        ));
+    }
 }
 add_action('enqueue_block_editor_assets', 'dblocks_codepro_enqueue_editor_assets');
