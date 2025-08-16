a639778cbfdd379dda56e070200be618d5469376
Marko Krstic <mailme@markokrstic.com>
2025-08-17 00:21:45 +0200

    Update enqueue-scripts.php

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -19,21 +19,8 @@ function dblocks_codepro_enqueue_scripts() {
             }
         }
 
-        if ($syntax_highlight_active) {
-            $syntax_theme_setting = get_option('dblocks_codepro_syntax_theme', 'light');
-            $style_url = $syntax_theme_setting === 'dark' ? 
-                         DBLOCKS_CODEPRO_URL . 'vendor/highlight/styles/vs2015.min.css' :
-                         DBLOCKS_CODEPRO_URL . 'vendor/highlight/styles/vs.min.css';
-            wp_enqueue_style('highlightjs-css', $style_url, array(), '1.0', 'all');
-            wp_enqueue_script('highlightjs', DBLOCKS_CODEPRO_URL . 'vendor/highlight/highlight.min.js', array(), '1.0', true);
-
-            $languages = ['xml', 'css', 'scss', 'javascript', 'php', 'typescript', 'bash', 'twig', 'yaml', 'plaintext', 'json'];
-            foreach ($languages as $lang) {
-                wp_enqueue_script("highlightjs-lang-$lang", DBLOCKS_CODEPRO_URL . "vendor/highlight/languages/$lang.min.js", array('highlightjs'), '1.0', true);
-            }
-
-            wp_add_inline_script('highlightjs', 'hljs.highlightAll();');
-        }
+        // No need to load highlight.js since Monaco editor handles all syntax highlighting
+        // This eliminates unnecessary HTTP requests and reduces bundle size
     }
 }
 add_action('wp_enqueue_scripts', 'dblocks_codepro_enqueue_scripts');
