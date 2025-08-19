537caf7fcd797116cb5a742aaa5c707f906d542b
Marko Krstic <mailme@markokrstic.com>
2025-08-19 20:43:31 +0200

    removed _arc

--- _arch/src/block-variations/component/CodeExecutorIcon.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CodeIcon } from '../../icons';
-
-const CodeExecutorIcon = () => <CodeIcon />;
-
-export default CodeExecutorIcon;
--- _arch/src/block-variations/component/SyntaxHighlighterIcon.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { HighlightIcon } from '../../icons';
-
-const SyntaxHighlighterIcon = () => <HighlightIcon />;
-
-export default SyntaxHighlighterIcon;
